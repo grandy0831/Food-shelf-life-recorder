ebe320d4f534dc3ac7b974d7d14e04e35f26ed33
Guandi Chen <140076679+grandy0831@users.noreply.github.com>
2024-02-28 15:47:18 +0000

    Update main.dart

--- SeatMap/seatmap/lib/main.dart
+++ SeatMap/seatmap/lib/main.dart
@@ -32,14 +32,14 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFA3DDEA),
+      backgroundColor: const Color(0xFFA3DDEA),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             // show icon
             Image.asset('assets/images/icon.jpg', width: 250, height: 250),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
           ],
         ),
       ),
@@ -72,7 +72,7 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'SeatMap',
           style: TextStyle(
             fontSize: 24.0, 
@@ -81,10 +81,10 @@ class _MainScreenState extends State<MainScreen> {
             // fontFamily: 'YourCustomFont', //字体样式
           ),
         ),
-        backgroundColor: Color(0xFFA3DDEA), 
+        backgroundColor: const Color(0xFFA3DDEA), 
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.settings),
+            icon: const Icon(Icons.settings),
             color: Colors.white, 
             onPressed: () {
               // Jump to the Settings page
@@ -112,10 +112,10 @@ class _MainScreenState extends State<MainScreen> {
                   hintText: 'Search...',
                   fillColor: Colors.white,
                   filled: true,
-                  prefixIcon: Icon(Icons.search),
+                  prefixIcon: const Icon(Icons.search),
                   suffixIcon: _searchController.text.isNotEmpty
                       ? IconButton(
-                          icon: Icon(Icons.cancel),
+                          icon: const Icon(Icons.cancel),
                           onPressed: () {
                             _searchController.clear();
                             setState(() {});
@@ -136,7 +136,7 @@ class _MainScreenState extends State<MainScreen> {
               padding: const EdgeInsets.all(8.0),
               children: <Widget>[
                 buildCard(context, 'UCL East - One Pool Street', '1 Pool St, London E20 2AF', 'Seat availability here...', 'Map for OPS'),
-                SizedBox(height: 8),
+                const SizedBox(height: 8),
                 buildCard(context, 'UCL East - Marshgate', '7 Sidings St, London E20 2AE', 'Seat availability here...', 'Map for Marshgate'),
               ],
             ),
@@ -146,33 +146,74 @@ class _MainScreenState extends State<MainScreen> {
     );
   }
 
-  Widget buildCard(BuildContext context, String buildingName, String address, String seatAvailability, String mapText) {
+ Widget buildCard(BuildContext context, String buildingName, String address, String seatAvailability, String mapText) {
     return Card(
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            title: Text(buildingName, style: TextStyle(fontWeight: FontWeight.bold)),
-            subtitle: Text(address),
-            trailing: IconButton(
-              icon: Icon(Icons.chevron_right),
-              onPressed: () {
-                // TODO: Navigate to building details page
-              },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0), 
+                      child: Text(
+                        buildingName,
+                        style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0), 
+                      child: Text(
+                        address,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              IconButton(
+                icon: const Icon(Icons.chevron_right),
+                onPressed: () {
+                  // TODO: Navigate to building details page
+                },
+              ),
+            ],
+          ),
+            const Divider(),
+            ListTile(
+              title: Text(seatAvailability),
+              // TODO: Show actual seat availability from API
             ),
-          ),
-          Divider(),
-          ListTile(
-            title: Text(seatAvailability),
-            // TODO: Show actual seat availability from API
-          ),
-          ListTile(
-            title: Text('Click here for map'),
-            trailing: Icon(Icons.location_on ),
-            onTap: () {
-              // TODO: Navigate to map screen
-            },
-          ),
-        ],
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MapScreen()),
+                );
+              },
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 8.0), // 在这里调整文本的缩进
+                      child: Text('Click here for map', style: TextStyle(fontSize: 16)),
+                    ),
+                    Icon(Icons.location_on),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -191,10 +232,25 @@ class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Settings"),
+        title: const Text("Settings"),
       ),
-      body: Center(
+      body: const Center(
         child: Text("Settings Page"),
+      ),
+    );
+  }
+}
+
+class MapScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Map"),
+        backgroundColor: const Color(0xFFA3DDEA), // 可以根据需要调整颜色
+      ),
+      body: const Center(
+        child: Text("Map content or widget goes here."),
       ),
     );
   }
