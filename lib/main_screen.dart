26aad61b3f85f0f009efdf4e6e184e5fbb11d4c1
Guandi Chen <140076679+grandy0831@users.noreply.github.com>
2024-04-05 22:20:18 +0100

    Update main_screen.dart

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'map_screen.dart'; 
+import 'one_pool_street_map_screen.dart'; 
+import 'marshgate_map_screen.dart'; 
 import 'settings_screen.dart'; 
 
 
 class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
   @override
   _MainScreenState createState() => _MainScreenState();
 }
@@ -21,136 +24,151 @@ class _MainScreenState extends State<MainScreen> {
     setState(() {});
   }
 
-@override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'SeatMap',
-          style: TextStyle(
-            fontSize: 24.0, 
-            fontWeight: FontWeight.bold, 
-            color: Colors.white, 
-            // fontFamily: 'YourCustomFont', //字体样式
+  @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'SeatMap',
+            style: TextStyle(
+              fontSize: 24.0, 
+              fontWeight: FontWeight.bold, 
+              color: Colors.white, 
+              // fontFamily: 'YourCustomFont', //字体样式
+            ),
           ),
+          backgroundColor: const Color.fromARGB(255, 80, 6, 119), 
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.settings),
+              color: Colors.white, 
+              onPressed: () {
+                // Jump to the Settings page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SettingsScreen()),
+                );
+              },
+            ),
+          ],
         ),
-        backgroundColor: const Color(0xFFA3DDEA), 
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.settings),
-            color: Colors.white, 
-            onPressed: () {
-              // Jump to the Settings page
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SettingsScreen()),
-              );
-            },
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 48.0,
-              child: TextField(
-                controller: _searchController,
-                onChanged: (value) {
-                  // Update UI
-                  setState(() {});
-                },
-                decoration: InputDecoration(
-                  hintText: 'Search...',
-                  fillColor: Colors.white,
-                  filled: true,
-                  prefixIcon: const Icon(Icons.search),
-                  suffixIcon: _searchController.text.isNotEmpty
-                      ? IconButton(
-                          icon: const Icon(Icons.cancel),
-                          onPressed: () {
-                            _searchController.clear();
-                            setState(() {});
-                          },
-                        )
-                      : null,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    borderSide: BorderSide.none,
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 48.0,
+                child: TextField(
+                  controller: _searchController,
+                  onChanged: (value) {
+                    // Update UI
+                    setState(() {});
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Search...',
+                    fillColor: Colors.white,
+                    filled: true,
+                    prefixIcon: const Icon(Icons.search),
+                    suffixIcon: _searchController.text.isNotEmpty
+                        ? IconButton(
+                            icon: const Icon(Icons.cancel),
+                            onPressed: () {
+                              _searchController.clear();
+                              setState(() {});
+                            },
+                          )
+                        : null,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide.none,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          
-          Expanded(
+            
+            Expanded(
             child: ListView(
               padding: const EdgeInsets.all(8.0),
               children: <Widget>[
-                buildCard(context, 'UCL East - One Pool Street', '1 Pool St, London E20 2AF', 'Seat availability here...', 'Map for OPS'),
+                buildCard(
+                  context, 
+                  'UCL East - One Pool Street', 
+                  '1 Pool St, London E20 2AF', 
+                  'Seat availability here...', 
+                  'Map for OPS', 
+                  () => Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const OnePoolStreetMapScreen()),
+                  ),
+                ),
                 const SizedBox(height: 8),
-                buildCard(context, 'UCL East - Marshgate', '7 Sidings St, London E20 2AE', 'Seat availability here...', 'Map for Marshgate'),
+                buildCard(
+                  context, 
+                  'UCL East - Marshgate', 
+                  '7 Sidings St, London E20 2AE', 
+                  'Seat availability here...', 
+                  'Map for Marshgate',
+                  () => Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MarshgateMapScreen()),
+                  ),
+                ),
               ],
             ),
           ),
-        ],
-      ),
-    );
-  }
+          ],
+        ),
+      );
+    }
 
- Widget buildCard(BuildContext context, String buildingName, String address, String seatAvailability, String mapText) {
+  Widget buildCard(BuildContext context, String buildingName, String address, String seatAvailability, String mapText, void Function() onTap) {
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0), 
-                      child: Text(
-                        buildingName,
-                        style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Text(
+                          buildingName,
+                          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 8),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0), 
-                      child: Text(
-                        address,
-                        style: const TextStyle(fontSize: 16),
+                      const SizedBox(height: 8),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Text(
+                          address,
+                          style: const TextStyle(fontSize: 16),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              IconButton(
-                icon: const Icon(Icons.chevron_right),
-                onPressed: () {
-                  // TODO: Navigate to building details page
-                },
-              ),
-            ],
-          ),
+                IconButton(
+                  icon: const Icon(Icons.chevron_right),
+                  onPressed: () {
+                    // If you have specific navigation logic for the button, add it here.
+                  },
+                ),
+              ],
+            ),
             const Divider(),
             ListTile(
               title: Text(seatAvailability),
-              // TODO: Show actual seat availability from API
+              // Add your seat availability logic here if necessary.
             ),
             InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MapScreen()),
-                );
-              },
+              onTap: onTap,
               child: const Padding(
                 padding: EdgeInsets.symmetric(vertical: 8.0),
                 child: Row(
@@ -161,7 +179,7 @@ class _MainScreenState extends State<MainScreen> {
                       child: Text('Click here for map', style: TextStyle(fontSize: 16)),
                     ),
                     Padding(
-                      padding: EdgeInsets.only(right: 12.0), 
+                      padding: EdgeInsets.only(right: 12.0),
                       child: Icon(Icons.location_on),
                     ),
                   ],
@@ -173,6 +191,8 @@ class _MainScreenState extends State<MainScreen> {
       ),
     );
   }
+
+
 
 
   @override
