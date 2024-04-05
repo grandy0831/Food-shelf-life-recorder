fbc9729ac3a8e65ae697b04d6eaec2923e6dd250
Guandi Chen <140076679+grandy0831@users.noreply.github.com>
2024-04-05 22:21:45 +0100

    Update splash_screen.dart

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'main_screen.dart'; 
 
 class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
@@ -11,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 1 ), () {
+    Timer(const Duration(seconds: 2 ), () {
       Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => MainScreen()));
     });
   }
@@ -19,13 +21,13 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFA3DDEA),
+      backgroundColor: const Color(0xFFA3DDEA),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Image.asset('assets/images/icon.jpg', width: 250, height: 250),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
           ],
         ),
       ),
