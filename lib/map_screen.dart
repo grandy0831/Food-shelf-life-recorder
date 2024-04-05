9586492c8adb43a45d55bad7897ec99bf0ede530
Guandi Chen <140076679+grandy0831@users.noreply.github.com>
2024-04-05 22:21:03 +0100

    Update map_screen.dart

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 class MapScreen extends StatelessWidget {
+  const MapScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Map"),
-        backgroundColor: Color(0xFFA3DDEA),
+        title: const Text("Map"),
+        backgroundColor: const Color.fromARGB(255, 80, 6, 119),
       ),
-      body: Center(
+      body: const Center(
         // 在这里放置实际的地图小部件，例如 Google Maps 或其他地图服务
         child: Text("Map content or widget goes here."),
       ),
