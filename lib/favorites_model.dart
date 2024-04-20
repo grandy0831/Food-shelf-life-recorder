dfdbf0526f744c91baaba9de0dcf7a2c161ba20d
Guandi Chen <140076679+grandy0831@users.noreply.github.com>
2024-04-20 08:58:50 +0100

    update favorites

--- lib/favorites_model.dart
+++ lib/favorites_model.dart
@@ -1,38 +1,56 @@
 import 'package:flutter/material.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FavoritesModel extends ChangeNotifier {
   final List<String> _favoriteBuildings = [];
   final List<String> _favoriteRooms = [];
 
+  FavoritesModel() {
+    _loadFavorites();
+  }
+
   List<String> get favoriteBuildings => List.unmodifiable(_favoriteBuildings);
   List<String> get favoriteRooms => List.unmodifiable(_favoriteRooms);
-
 
   void addBuilding(String building) {
     if (!_favoriteBuildings.contains(building)) {
       _favoriteBuildings.add(building);
-      notifyListeners(); 
+      notifyListeners();
+      _saveFavorites('favoriteBuildings', _favoriteBuildings);
     }
   }
 
   void removeBuilding(String building) {
     if (_favoriteBuildings.remove(building)) {
-      notifyListeners(); 
+      notifyListeners();
+      _saveFavorites('favoriteBuildings', _favoriteBuildings);
     }
   }
 
   void addRoom(String room) {
     if (!_favoriteRooms.contains(room)) {
       _favoriteRooms.add(room);
-      notifyListeners(); 
+      notifyListeners();
+      _saveFavorites('favoriteRooms', _favoriteRooms);
     }
   }
 
   void removeRoom(String room) {
     if (_favoriteRooms.remove(room)) {
-      notifyListeners(); 
+      notifyListeners();
+      _saveFavorites('favoriteRooms', _favoriteRooms);
     }
   }
 
+  Future<void> _loadFavorites() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    _favoriteBuildings.addAll(prefs.getStringList('favoriteBuildings') ?? []);
+    _favoriteRooms.addAll(prefs.getStringList('favoriteRooms') ?? []);
+    notifyListeners();
+  }
+
+  Future<void> _saveFavorites(String key, List<String> data) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList(key, data);
+  }
 }
