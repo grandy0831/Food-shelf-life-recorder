81f067250111f09f6960c852df9f13beb5d31a1d
Guandi Chen <140076679+grandy0831@users.noreply.github.com>
2024-04-22 01:40:16 +0100

    Update api_secrets.dart

--- lib/api_secrets.dart
+++ lib/api_secrets.dart
@@ -1,8 +1,5 @@
 import 'dart:io' show Platform;
 
 class Secrets {
-  // static String get googleMapsApiKey => Platform.environment['GOOGLE_MAPS_API_KEY']!;
-  // static String get UCLApiKey => Platform.environment['UCL_API_KEY']!;
-  static String get googleMapsApiKey => Platform.environment['GOOGLE_MAPS_API_KEY']!;
-  static String get UCLApiKey => Platform.environment['UCL_API_KEY']!;
+    static string get UCLApiKey => UCL_API_KEY;
 }
