9b7f49e83069b3958142db9e6b2aec223cf13793
Guandi Chen <140076679+grandy0831@users.noreply.github.com>
2024-04-22 09:10:14 +0100

    Fix the bug of not storing locally

--- lib/study_space.dart
+++ lib/study_space.dart
@@ -14,9 +14,9 @@ class Room {
   Room({required this.roomId, required this.roomType});
 
   factory Room.fromJson(Map<String, dynamic> json, String roomNameKey) => Room(
-    roomId: json['room_id'] ?? 'Unknown ID',
-    roomType: json[roomNameKey] ?? 'Unknown Type',
-  );
+        roomId: json['room_id'] ?? 'Unknown ID',
+        roomType: json[roomNameKey] ?? 'Unknown Type',
+      );
 
   void addSeat(bool occupied) {
     totalSeats++;
@@ -24,15 +24,18 @@ class Room {
   }
 }
 
-Future<Map<String, List<Room>>> fetchRoomsForFloors(List<String> floorNames, String surveyId, String roomNameKey) async {
-  final response = await http.get(Uri.parse('https://uclapi.com/workspaces/sensors?survey_id=$surveyId&token=${Secrets.UCLApiKey}'));
+Future<Map<String, List<Room>>> fetchRoomsForFloors(
+    List<String> floorNames, String surveyId, String roomNameKey) async {
+  final response = await http.get(Uri.parse(
+      'https://uclapi.com/workspaces/sensors?survey_id=$surveyId&token=${Secrets.UCLApiKey}'));
   if (response.statusCode != 200) throw Exception('Failed to load room data');
 
   var data = jsonDecode(response.body);
   Map<String, List<Room>> floorRooms = {};
 
   for (var floorName in floorNames) {
-    var floorMap = data['maps'].firstWhere((map) => map['name'] == floorName, orElse: () => null);
+    var floorMap =
+        data['maps'].firstWhere((map) => map['name'] == floorName, orElse: () => null);
     if (floorMap == null) {
       floorRooms[floorName] = [];
       continue;
@@ -44,7 +47,8 @@ Future<Map<String, List<Room>>> fetchRoomsForFloors(List<String> floorNames, Str
       String roomType = sensor[roomNameKey] ?? 'Unknown Type';
       if (roomType == 'Unknown Type') continue;
 
-      rooms.putIfAbsent(roomType, () => Room.fromJson(sensor, roomNameKey)).addSeat(sensor['occupied'] ?? false);
+      rooms.putIfAbsent(
+          roomType, () => Room.fromJson(sensor, roomNameKey)).addSeat(sensor['occupied'] ?? false);
     }
 
     floorRooms[floorName] = rooms.values.toList();
@@ -81,8 +85,18 @@ class _MapScreenState extends State<MapScreen> {
 
   Future<void> _fetchRooms() async {
     try {
-      final floorRoomsBuilding1 = await fetchRoomsForFloors(["Floor 1", "Floor 2", "Floor 3", "Floor 4", "Floor 5", "Floor 6", "Floor 7", "Floor 8"], "115", "description_2");
-      final floorRoomsBuilding2 = await fetchRoomsForFloors(["Ground Floor", "First Floor", "Second Floor", "Third Floor"], "111", "description_1");
+      final floorRoomsBuilding1 = await fetchRoomsForFloors([
+        "Floor 1",
+        "Floor 2",
+        "Floor 3",
+        "Floor 4",
+        "Floor 5",
+        "Floor 6",
+        "Floor 7",
+        "Floor 8"
+      ], "115", "description_2");
+      final floorRoomsBuilding2 = await fetchRoomsForFloors(
+          ["Ground Floor", "First Floor", "Second Floor", "Third Floor"], "111", "description_1");
       setState(() {
         _floorRoomsBuilding1 = floorRoomsBuilding1;
         _floorRoomsBuilding2 = floorRoomsBuilding2;
@@ -110,11 +124,12 @@ class _MapScreenState extends State<MapScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final favorites = Provider.of<FavoritesModel>(context); 
+    final favorites = Provider.of<FavoritesModel>(context);
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Search",
+        title: const Text(
+          "Search",
           style: TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
@@ -128,85 +143,87 @@ class _MapScreenState extends State<MapScreen> {
       body: _isLoadingBuilding1 || _isLoadingBuilding2
           ? const Center(child: CircularProgressIndicator())
           : Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              height: 48.0,
-              child: TextField(
-                controller: _searchController,
-                onChanged: _searchRooms,
-                decoration: InputDecoration(
-                  hintText: 'Search...',
-                  fillColor: Colors.white,
-                  filled: true,
-                  prefixIcon: const Icon(Icons.search),
-                  suffixIcon: _searchController.text.isNotEmpty
-                      ? IconButton(
-                    icon: const Icon(Icons.cancel),
-                    onPressed: () {
-                      _searchController.clear();
-                      _searchRooms('');
-                    },
-                  )
-                      : null,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: RefreshIndicator(
-              onRefresh: _refreshRooms,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    if (_floorRoomsBuilding1 != null || _floorRoomsBuilding2 != null)
-                      ..._buildRoomsList(_floorRoomsBuilding1 ?? {}, _floorRoomsBuilding2 ?? {}, favorites) // 传递FavoritesModel
-                    else
-                      const Center(
-                        child: Text(
-                          "Oops! No matches found.",
-                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: 48.0,
+                    child: TextField(
+                      controller: _searchController,
+                      onChanged: _searchRooms,
+                      decoration: InputDecoration(
+                        hintText: 'Search...',
+                        fillColor: Colors.white,
+                        filled: true,
+                        prefixIcon: const Icon(Icons.search),
+                        suffixIcon: _searchController.text.isNotEmpty
+                            ? IconButton(
+                                icon: const Icon(Icons.cancel),
+                                onPressed: () {
+                                  _searchController.clear();
+                                  _searchRooms('');
+                                },
+                              )
+                            : null,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: BorderSide.none,
                         ),
                       ),
-                  ],
+                    ),
+                  ),
                 ),
-              ),
+                Expanded(
+                  child: RefreshIndicator(
+                    onRefresh: _refreshRooms,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          if (_floorRoomsBuilding1 != null || _floorRoomsBuilding2 != null)
+                            ..._buildRoomsList(
+                                _floorRoomsBuilding1 ?? {}, _floorRoomsBuilding2 ?? {}, favorites) // 传递FavoritesModel
+                          else
+                            const Center(
+                              child: Text(
+                                "Oops! No matches found.",
+                                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
     );
   }
 
-  List<Widget> _buildRoomsList(Map<String, List<Room>> floorRoomsBuilding1, Map<String, List<Room>> floorRoomsBuilding2, FavoritesModel favorites) {
+  List<Widget> _buildRoomsList(
+      Map<String, List<Room>> floorRoomsBuilding1, Map<String, List<Room>> floorRoomsBuilding2, FavoritesModel favorites) {
     List<Widget> roomCards = [];
     bool hasMatches = false;
 
     for (var floor in floorRoomsBuilding1.keys) {
-      var matchingRooms = floorRoomsBuilding1[floor]!.where((room) => room.roomType.toLowerCase().contains(_searchQuery.toLowerCase())).toList();
+      var matchingRooms = floorRoomsBuilding1[floor]!
+          .where((room) => room.roomType.toLowerCase().contains(_searchQuery.toLowerCase()))
+          .toList();
       if (matchingRooms.isNotEmpty) {
         hasMatches = true;
         roomCards.addAll(
-          matchingRooms
-              .map<Widget>((room) => _buildRoomCard(room, floor, favorites)) 
-              .toList(),
+          matchingRooms.map<Widget>((room) => _buildRoomCard(room, floor, favorites)).toList(),
         );
       }
     }
 
     for (var floor in floorRoomsBuilding2.keys) {
-      var matchingRooms = floorRoomsBuilding2[floor]!.where((room) => room.roomType.toLowerCase().contains(_searchQuery.toLowerCase())).toList();
+      var matchingRooms = floorRoomsBuilding2[floor]!
+          .where((room) => room.roomType.toLowerCase().contains(_searchQuery.toLowerCase()))
+          .toList();
       if (matchingRooms.isNotEmpty) {
         hasMatches = true;
         roomCards.addAll(
-          matchingRooms
-              .map<Widget>((room) => _buildRoomCard(room, floor, favorites)) 
-              .toList(),
+          matchingRooms.map<Widget>((room) => _buildRoomCard(room, floor, favorites)).toList(),
         );
       }
     }
@@ -233,62 +250,60 @@ class _MapScreenState extends State<MapScreen> {
     return roomCards;
   }
 
-Widget _buildRoomCard(Room room, String floor, FavoritesModel favorites) {
-  String roomName = room.roomType; 
-  String formattedFloor = _formatFloor(floor);
-  Color availabilityColor = _getAvailabilityColor(room);
-  bool isFavorite = favorites.isRoomFavorite(roomName); 
+  Widget _buildRoomCard(Room room, String floor, FavoritesModel favorites) {
+    String roomName = room.roomType;
+    String formattedFloor = _formatFloor(floor);
+    Color availabilityColor = _getAvailabilityColor(room);
+    bool isFavorite = favorites.isRoomFavorite(roomName);
 
-  return Card(
-    elevation: 2,
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
-    margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 12), 
-    child: ListTile(
-      contentPadding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12), 
-      title: Text('$roomName', style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 4), 
-          Text('$formattedFloor', style: TextStyle(fontSize: 16, color: Colors.grey[700])),
-          const SizedBox(height: 4), 
-          Row(
-            children: [
-              Container(
-                width: 60,
-                height: 25,
-                decoration: BoxDecoration(color: availabilityColor, borderRadius: BorderRadius.circular(6.0)),
-                alignment: Alignment.center,
-                child: const Text("Seats", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16)),
-              ),
-              Expanded(
-                child: Text(" ${room.totalSeats - room.occupiedSeats} available / ${room.totalSeats} total", style: const TextStyle(fontSize: 16)),
-              ),
-            ],
-          ),
-        ],
-      ),
-      trailing: IconButton(
-        icon: Icon(
-          isFavorite ? Icons.favorite : Icons.favorite_border,
-          color: isFavorite ? Colors.red : null,
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+      margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+      child: ListTile(
+        contentPadding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+        title: Text('$roomName', style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 4),
+            Text('$formattedFloor', style: TextStyle(fontSize: 16, color: Colors.grey[700])),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Container(
+                  width: 60,
+                  height: 25,
+                  decoration: BoxDecoration(color: availabilityColor, borderRadius: BorderRadius.circular(6.0)),
+                  alignment: Alignment.center,
+                  child: const Text("Seats", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16)),
+                ),
+                Expanded(
+                  child: Text(" ${room.totalSeats - room.occupiedSeats} available / ${room.totalSeats} total", style: const TextStyle(fontSize: 16)),
+                ),
+              ],
+            ),
+          ],
         ),
-        onPressed: () {
+        trailing: IconButton(
+          icon: Icon(
+            isFavorite ? Icons.favorite : Icons.favorite_border,
+            color: isFavorite ? Colors.red : null,
+          ),
+          onPressed: () {
+            _toggleFavorite(roomName, favorites); 
+          },
+        ),
+        onTap: () {
           _toggleFavorite(roomName, favorites); 
         },
       ),
-      onTap: () {
-        _toggleFavorite(roomName, favorites); 
-      },
-    ),
-  );
-}
-
-
+    );
+  }
 
 
   void _toggleFavorite(String roomName, FavoritesModel favorites) {
-    favorites.toggleRoomFavorite(roomName); 
+    favorites.toggleRoomFavorite(roomName);
   }
 
   String _formatFloor(String floor) {
@@ -310,8 +325,11 @@ Widget _buildRoomCard(Room room, String floor, FavoritesModel favorites) {
 
   Color _getAvailabilityColor(Room room) {
     double availablePercentage = (room.totalSeats - room.occupiedSeats) / room.totalSeats;
-    if (availablePercentage > 0.5) return Colors.green;
-    if (availablePercentage >= 0.2) return Colors.amber;
-    return Colors.red;
+    if (availablePercentage > 0.5)
+      return Colors.green;
+    else if (availablePercentage >= 0.2)
+      return Colors.amber;
+    else
+      return Colors.red;
   }
 }
